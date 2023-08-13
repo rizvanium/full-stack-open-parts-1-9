45b276e86c8e4199358a3b19aee64b82b2b0b5b2
Rizvan <rizvan.chalilovas@proton.me>
2023-08-13 22:24:51 +0300

    complete ex.: 2.11

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,15 +1,20 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 import InputField from './components/InputField';
 import NewPersonForm from './NewPersonForm';
 import PersonsList from './PersonsList';
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ]);
+  const [persons, setPersons] = useState([]);
+  console.log('before');
+  useEffect(() => {
+    console.log('inside');
+    axios.get('http://localhost:3001/persons').then(response => {
+      console.log('fullfiled');
+      setPersons(response.data);
+    });
+  }, []);
+
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
   const [searchPhrase, setSearchPhrase] = useState('');
@@ -42,6 +47,8 @@ const App = () => {
     setNewName('');
     setNewNumber('');
   }
+
+  console.log('after');
 
   return (
     <div>
