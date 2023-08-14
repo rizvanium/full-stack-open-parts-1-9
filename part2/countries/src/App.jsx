bd4bcf46cac99ab022a98a5476eaa5d1aef733b1
Rizvan <rizvan.chalilovas@proton.me>
2023-08-14 21:37:50 +0300

    complete ex.: [2.18]

--- part2/countries/src/App.jsx
+++ part2/countries/src/App.jsx
@@ -11,16 +11,25 @@ function App() {
 
   useEffect(() => {
 
-    if (!searchWord) return;
+    if (!searchWord || searchWord == '') {
+      setCountries([]);
+      return;
+    }
 
     countryService
       .getAll()
       .then(data => {
-        const matchedCountries = data.filter(country => 
-          country.name.common
+        const matchedCountries = data
+          .filter(country => country.name.common
             .toLowerCase()
-            .includes(searchWord.toLowerCase()));
-        console.log(matchedCountries);
+            .includes(searchWord.toLowerCase()))
+          .map(country => ({
+            name: country.name.common,
+            capitals: country.capital,
+            area: country.area,
+            languages: country.languages,
+            flags: country.flags
+          }));
         setCountries([...matchedCountries]);
       })
       .catch(err => console.log(err));
@@ -30,8 +39,37 @@ function App() {
   return (
     <div>
       <p>find countries <input type='text' onChange={handleInputChange} /></p>
+      { countries && 
+        (countries.length === 1 ? <CountryDetails country={countries[0]} /> : <CountryList countries={countries}/>) 
+      }
     </div>
   )
+}
+
+const CountryList = ({ countries }) => {
+  if (countries.length > 10) {
+    return <p>Too many matches, specify another filter</p>
+  }
+
+  return <div>
+    {countries.map(country => <p key={country.area}>{country.name}</p>)}
+  </div>
+}
+
+const CountryDetails = ({ country }) => {
+  if (!country) {
+    return null;
+  }
+  return (<div>
+    <h2>{country.name}</h2>
+    <p>capital: {country.capitals.join(', ')}</p>
+    <p>area: {country.area}</p>
+    <h3>languages:</h3>
+    <ul>
+      { Object.values(country.languages).map(lang => <li key={lang}>{lang}</li>) }
+    </ul>
+    <img src={country.flags.png} alt={country.flags.alt}/>
+  </div>)
 }
 
 export default App
