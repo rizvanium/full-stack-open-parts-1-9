eb6d4911d490c86644aedd35acf1b810f90bcd4e
Rizvan <rizvan.chalilovas@proton.me>
2023-08-16 12:40:25 +0300

    change page title and backend service url

--- part2/phonebook/src/services/persons.js
+++ part2/phonebook/src/services/persons.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const BACKEND_BASE_URL = 'http://localhost:3001';
-const PERSONS_URL = `${BACKEND_BASE_URL}/persons`;
+const BACKEND_BASE_URL = '';
+const PERSONS_URL = `${BACKEND_BASE_URL}/api/persons`;
 
 const getAll = () => {
   return axios
