42d75e14333f47dccc4333a16d93dfccbc8e40bb
Rizvan <rizvan.chalilovas@proton.me>
2023-08-19 12:02:35 +0300

    bloglist: add logger

--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -1,15 +1,18 @@
 const express = require('express');
-const app = express();
 const cors = require('cors');
-const Blog = require('./models/blog');
 const mongoose = require('mongoose');
+const logger = require('./utils/logger');
+const Blog = require('./models/blog');
+const app = express();
 
 const mongoUrl = 'mongodb://localhost/bloglist';
 mongoose.connect(mongoUrl)
   .then(() => {
-    console.log('connected to MongoDB!');
+    logger.info('connected to MongoDB!');
   })
-  .catch((error) => console.error('error connecting to MongoDB:', error.message));
+  .catch((error) => {
+    logger.error('error connecting to MongoDB:', error.message);
+  });
 
 app.use(cors());
 app.use(express.json());
@@ -34,5 +37,5 @@ app.post('/api/blogs', (request, response) => {
 
 const PORT = 3003;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  logger.info(`Server running on port ${PORT}`);
 });
