647034bf1875bb27d1db2ee36913b38efe6ca00b
Rizvan <rizvan.chalilovas@proton.me>
2023-08-19 12:46:02 +0300

    bloglist: split router from the main app

--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -3,9 +3,9 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 const logger = require('./utils/logger');
 const config = require('./utils/config');
-const Blog = require('./models/blog');
-const app = express();
+const blogsRouter = require('./controllers/blogs');
 
+const app = express();
 mongoose.connect(config.MONGODB_URI)
   .then(() => {
     logger.info('connected to MongoDB!');
@@ -16,24 +16,7 @@ mongoose.connect(config.MONGODB_URI)
 
 app.use(cors());
 app.use(express.json());
-
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs);
-    });
-});
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body);
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result);
-    });
-});
+app.use('/api/blogs', blogsRouter);
 
 app.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`);
