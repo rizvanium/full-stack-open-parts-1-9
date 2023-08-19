a898ad7a1919cd5817c3bb1ed99db005d9424c54
Rizvan <rizvan.chalilovas@proton.me>
2023-08-19 11:44:08 +0300

    bloglist: change eslint rules and fix the errors

--- part4/bloglist/.eslintrc.js
+++ part4/bloglist/.eslintrc.js
@@ -1,42 +1,50 @@
 module.exports = {
-    'env': {
-        'browser': true,
-        'commonjs': true,
-        'es2021': true
-    },
-    'extends': 'eslint:recommended',
-    'overrides': [
-        {
-            'env': {
-                'node': true
-            },
-            'files': [
-                '.eslintrc.{js,cjs}'
-            ],
-            'parserOptions': {
-                'sourceType': 'script'
-            }
-        }
-    ],
-    'parserOptions': {
-        'ecmaVersion': 'latest'
-    },
-    'rules': {
-        'indent': [
-            'error',
-            4
-        ],
-        'linebreak-style': [
-            'error',
-            'unix'
-        ],
-        'quotes': [
-            'error',
-            'single'
-        ],
-        'semi': [
-            'error',
-            'always'
-        ]
+  'env': {
+    'browser': true,
+    'commonjs': true,
+    'es2021': true
+  },
+  'extends': 'eslint:recommended',
+  'overrides': [
+    {
+      'env': {
+        'node': true
+      },
+      'files': [
+        '.eslintrc.{js,cjs}'
+      ],
+      'parserOptions': {
+        'sourceType': 'script'
+      }
     }
+  ],
+  'parserOptions': {
+    'ecmaVersion': 'latest'
+  },
+  'rules': {
+    'indent': [
+      'error',
+      2
+    ],
+    'linebreak-style': [
+      'error',
+      'unix'
+    ],
+    'quotes': [
+      'error',
+      'single'
+    ],
+    'semi': [
+      'error',
+      'always'
+    ],
+    'no-trailing-spaces': 'error',
+    'object-curly-spacing': [
+      'error', 'always'
+    ],
+    'arrow-spacing': [
+      'error', { 'before': true, 'after': true }
+    ],
+    'no-console': 0,
+  }
 };
--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -4,10 +4,10 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 
 const blogSchema = new mongoose.Schema({
-    title: String,
-    author: String,
-    url: String,
-    likes: Number
+  title: String,
+  author: String,
+  url: String,
+  likes: Number
 });
 
 const Blog = mongoose.model('Blog', blogSchema);
@@ -19,24 +19,24 @@ app.use(cors());
 app.use(express.json());
 
 app.get('/api/blogs', (request, response) => {
-    Blog
-        .find({})
-        .then(blogs => {
-            response.json(blogs);
-        });
+  Blog
+    .find({})
+    .then(blogs => {
+      response.json(blogs);
+    });
 });
 
 app.post('/api/blogs', (request, response) => {
-    const blog = new Blog(request.body);
+  const blog = new Blog(request.body);
 
-    blog
-        .save()
-        .then(result => {
-            response.status(201).json(result);
-        });
+  blog
+    .save()
+    .then(result => {
+      response.status(201).json(result);
+    });
 });
 
 const PORT = 3003;
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
