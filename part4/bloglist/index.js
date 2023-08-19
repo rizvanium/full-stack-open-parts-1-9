60347067ba599f32092433bcaa12312d4a53f88d
Rizvan <rizvan.chalilovas@proton.me>
2023-08-19 13:00:45 +0300

    bloglist: move app setup logic into different module

--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -1,22 +1,8 @@
-const express = require('express');
-const cors = require('cors');
 const mongoose = require('mongoose');
 const logger = require('./utils/logger');
 const config = require('./utils/config');
-const blogsRouter = require('./controllers/blogs');
+const app = require('./app');
 
-const app = express();
-mongoose.connect(config.MONGODB_URI)
-  .then(() => {
-    logger.info('connected to MongoDB!');
-  })
-  .catch((error) => {
-    logger.error('error connecting to MongoDB:', error.message);
-  });
-
-app.use(cors());
-app.use(express.json());
-app.use('/api/blogs', blogsRouter);
 
 app.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`);
