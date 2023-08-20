08c9beb3238c1efe239d535abbbb9a86b15baa74
Rizvan <rizvan.chalilovas@proton.me>
2023-08-20 17:08:09 +0300

    bloglist: refactor GET /api/blogs route handler to use async/await syntax

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -1,17 +1,17 @@
 const blogsRouter = require('express').Router();
 const Blog = require('../models/blog');
 
-blogsRouter.get('/', (request, response, next) => {
-  Blog.find({})
-    .then((blogs) => {
-      response.json(blogs);
-    })
-    .catch((error) => next(error));
+blogsRouter.get('/', async (request, response, next) => {
+  try {
+    const blogs = await Blog.find({});
+    response.json(blogs);
+  } catch (error) {
+    next(error);
+  }
 });
 
 blogsRouter.post('/', (request, response, next) => {
   const blog = new Blog(request.body);
-
   blog
     .save()
     .then((result) => {
