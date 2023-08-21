c8401fc2ab6ea6b68133e46b560a3d0828354393
Rizvan <rizvan.chalilovas@proton.me>
2023-08-21 22:33:13 +0300

    bloglist: setup correct blog list population on user

--- part4/bloglist/controllers/users.js
+++ part4/bloglist/controllers/users.js
@@ -4,7 +4,11 @@ const User = require('../models/user');
 
 usersRouter.get('/', async (request, response, next) => {
   try {
-    const users = await User.find({}).populate('blogs');
+    const users = await User.find({}).populate('blogs', {
+      url: 1,
+      title: 1,
+      author: 1,
+    });
     response.json(users);
   } catch (error) {
     next(error);
