08af178ad28b0f8dfa2eeb432f3b064e625cd6e4
Rizvan <rizvan.chalilovas@proton.me>
2023-08-28 21:48:30 +0300

    bloglist-fe: add login form

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -4,6 +4,9 @@ import blogService from './services/blogs'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [user, setUser] = useState(null);
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -11,12 +14,47 @@ const App = () => {
     )  
   }, [])
 
-  return (
+  const handleLogin = () => {
+    
+  }
+
+  const loginForm = () => (    
+  <form onSubmit={handleLogin}>
+      <div>
+        username
+          <input
+          type="text"
+          value={username}
+          name="Username"
+          onChange={({ target }) => setUsername(target.value)}
+        />
+      </div>
+      <div>
+        password
+          <input
+          type="password"
+          value={password}
+          name="Password"
+          onChange={({ target }) => setPassword(target.value)}
+        />
+      </div>
+      <button type="submit">login</button>
+    </form>  
+    );
+
+  const blogList = () => (
     <div>
       <h2>blogs</h2>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
+    </div>
+  );
+
+  return (
+    <div>
+      {user == null && loginForm()}
+      {user != null && blogList()}
     </div>
   )
 }
