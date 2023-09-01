c1da2affd0905cdd84a5d0f917441f04ab95a291
Rizvan <rizvan.chalilovas@proton.me>
2023-09-01 21:44:03 +0300

    bloglist-fe - blogs are now sorted by the number of likes

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -123,9 +123,11 @@ const App = () => {
         {user.name} logged in&nbsp;
         <button onClick={handleLogout}>logout</button>
       </p>
-      {blogs.map((blog) => (
-        <Blog key={blog.id} blog={blog} handleBlogUpdate={updateBlog} />
-      ))}
+      {blogs
+        .sort((a, b) => b.likes - a.likes)
+        .map((blog) => (
+          <Blog key={blog.id} blog={blog} handleBlogUpdate={updateBlog} />
+        ))}
     </div>
   );
 
