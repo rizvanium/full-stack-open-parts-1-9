652c5f8472f5c82a855acbb83c8a9fc480678e37
Rizvan <rizvan.chalilovas@proton.me>
2023-09-01 17:57:21 +0300

    bloglist-fe - add blog details

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,30 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-);
+import { useState } from 'react';
+
+const Blog = ({ blog }) => {
+  const [showDetails, setShowDetails] = useState(false);
+  const buttonText = showDetails ? 'hide' : 'view';
+  const displayDetails = { display: showDetails ? '' : 'none' };
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5,
+  };
+
+  return (
+    <div style={blogStyle}>
+      {blog.title} {blog.author}{' '}
+      <button onClick={() => setShowDetails(!showDetails)}>{buttonText}</button>
+      <div style={displayDetails}>
+        <p>{blog.url}</p>
+        <p>{blog.user.name}</p>
+        <p>
+          likes {blog.likes} <button>like</button>
+        </p>
+      </div>
+    </div>
+  );
+};
 
 export default Blog;
