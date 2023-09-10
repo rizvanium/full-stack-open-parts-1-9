f525fbbb28e813178ddbfcea22ee53096fb75ce3
Rizvan <rizvan.chalilovas@proton.me>
2023-09-10 11:22:36 +0300

    redux-anecdotes - ex.: 6.7 move anecdote creation into different component

--- part6/redux-anecdotes/src/App.js
+++ part6/redux-anecdotes/src/App.js
@@ -1,5 +1,6 @@
 import { useSelector, useDispatch } from 'react-redux'
-import { increaseVoteCount, createAnecdote } from './reducers/anecdoteReducer';
+import { increaseVoteCount } from './reducers/anecdoteReducer';
+import AnecdoteForm from './components/AnecdoteForm';
 
 const App = () => {
   const anecdotes = useSelector(state => state)
@@ -8,13 +9,6 @@ const App = () => {
   const vote = (id) => {
     console.log('vote', id)
     dispatch(increaseVoteCount(id));
-  }
-
-  const addNewAnecdote = (event) => {
-    event.preventDefault();
-    const anecdote = event.target.anecdote.value;
-    event.target.anecdote.value = '';
-    dispatch(createAnecdote(anecdote))
   }
 
   return (
@@ -31,11 +25,7 @@ const App = () => {
           </div>
         </div>
       )}
-      <h2>create new</h2>
-      <form onSubmit={addNewAnecdote}>
-        <div><input name="anecdote"/></div>
-        <button>create</button>
-      </form>
+      <AnecdoteForm />
     </div>
   )
 }
