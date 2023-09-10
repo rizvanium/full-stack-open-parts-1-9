c3103161c6bcc8405c626ea0be67e870a8c8416d
Rizvan <rizvan.chalilovas@proton.me>
2023-09-10 17:51:02 +0300

    redux-anecdotes - ex.: 6.16 app initializes anecdotes using async action creators

--- part6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,4 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit"
+import anecdoteService from '../services/anecdotes';
 
 const anecdoteSlice = createSlice({
   name: 'anecdotes',
@@ -24,3 +25,8 @@ const anecdoteSlice = createSlice({
 
 export const { createAnecdote, increaseVoteCount, setAnecdotes } = anecdoteSlice.actions
 export default anecdoteSlice.reducer
+
+export const initializeAnecdotes = () => async (dispatch) => {
+  const anecdotes = await anecdoteService.getAll();
+  dispatch(setAnecdotes(anecdotes));
+}
