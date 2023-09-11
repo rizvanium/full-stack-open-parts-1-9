5229599d6e28f9fe1dc61eae69d74e08d0530bd4
Rizvan <rizvan.chalilovas@proton.me>
2023-09-11 21:44:44 +0300

    query-anecdotes - 6.21 implement new anecdote creation

--- part6/query-anecdotes/src/components/AnecdoteForm.jsx
+++ part6/query-anecdotes/src/components/AnecdoteForm.jsx
@@ -1,10 +1,19 @@
-const AnecdoteForm = () => {
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+import { createAnecdote } from '../requests';
 
+const AnecdoteForm = () => {
+  const queryClient = useQueryClient();
+  const newAnecdoteMutation = useMutation(createAnecdote, {
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['anecdotes'] })
+    }
+  })
   const onCreate = (event) => {
     event.preventDefault()
     const content = event.target.anecdote.value
     event.target.anecdote.value = ''
     console.log('new anecdote')
+    newAnecdoteMutation.mutate({ content, votes: 0 });
 }
 
   return (
