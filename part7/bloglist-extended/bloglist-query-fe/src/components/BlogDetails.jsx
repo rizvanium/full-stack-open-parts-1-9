ddaa02decc6700c7a87c9796da2c372b883094da
Rizvan <rizvan.chalilovas@proton.me>
2023-09-18 10:44:05 +0300

    bloglist-query-fe - update BlogDetails to display user comments

--- part7/bloglist-extended/bloglist-query-fe/src/components/BlogDetails.jsx
+++ part7/bloglist-extended/bloglist-query-fe/src/components/BlogDetails.jsx
@@ -1,8 +1,9 @@
+import { useState } from 'react';
 import { useUserValue } from '../UserContext';
 
 const BlogDetails = ({ blog, handleUpdate, handleRemoval }) => {
   const currentUser = useUserValue();
-
+  const [newComment, setNewComment] = useState('');
   const addOneLike = () => {
     handleUpdate({
       ...blog,
@@ -14,6 +15,25 @@ const BlogDetails = ({ blog, handleUpdate, handleRemoval }) => {
     if (confirm(`Remove blog ${blog.title} by ${blog.author}`)) {
       handleRemoval(blog.id);
     }
+  };
+
+  const addComment = (event) => {
+    event.preventDefault();
+    console.log('added new comment', newComment);
+  };
+
+  const commentList = () => {
+    if (blog.comments.length === 0) {
+      return <p>no comments</p>;
+    }
+
+    return (
+      <ul>
+        {blog.comments.map((comment) => (
+          <li key={comment.id}>{comment.text}</li>
+        ))}
+      </ul>
+    );
   };
 
   return (
@@ -31,6 +51,16 @@ const BlogDetails = ({ blog, handleUpdate, handleRemoval }) => {
           remove
         </button>
       )}
+      <h3>comments</h3>
+      <form onSubmit={addComment}>
+        <input
+          type="text"
+          value={newComment}
+          onChange={({ target }) => setNewComment(target.value)}
+        />
+        <button type="submit">add comment</button>
+      </form>
+      {commentList()}
     </div>
   );
 };
