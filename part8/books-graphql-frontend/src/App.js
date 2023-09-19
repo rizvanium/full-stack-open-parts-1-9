54596548b664da4730831d93567851eb8561e66e
Rizvan <rizvan.chalilovas@proton.me>
2023-09-19 14:58:48 +0300

    books-graphql-frontend - refactor app to use react router

--- part8/books-graphql-frontend/src/App.js
+++ part8/books-graphql-frontend/src/App.js
@@ -2,23 +2,28 @@ import { useState } from 'react';
 import Authors from './components/Authors';
 import Books from './components/Books';
 import NewBook from './components/NewBook';
+import { Route, Routes, useNavigate } from 'react-router-dom';
 
 const App = () => {
-  const [page, setPage] = useState('authors');
+  const navigate = useNavigate();
 
   return (
     <div>
-      <div>
-        <button onClick={() => setPage('authors')}>authors</button>
-        <button onClick={() => setPage('books')}>books</button>
-        <button onClick={() => setPage('add')}>add book</button>
-      </div>
+      <header>
+        <nav>
+          <button onClick={() => navigate('/')}>authors</button>
+          <button onClick={() => navigate('/books')}>books</button>
+          <button onClick={() => navigate('/books/new')}>add book</button>
+        </nav>
+      </header>
 
-      <Authors show={page === 'authors'} />
-
-      <Books show={page === 'books'} />
-
-      <NewBook show={page === 'add'} />
+      <main>
+        <Routes>
+          <Route path="/" element={<Authors />}></Route>
+          <Route path="/books" element={<Books />}></Route>
+          <Route path="/books/new" element={<NewBook />}></Route>
+        </Routes>
+      </main>
     </div>
   );
 };
