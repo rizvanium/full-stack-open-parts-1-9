573d463edc2586cd78ed8c02e02097ffcfa59b02
Rizvan <rizvan.chalilovas@proton.me>
2023-09-19 14:49:15 +0300

    books-graphql-frontend - add prettier config and fix all errors/warnings

--- part8/books-graphql-frontend/src/App.js
+++ part8/books-graphql-frontend/src/App.js
@@ -1,10 +1,10 @@
-import { useState } from 'react'
-import Authors from './components/Authors'
-import Books from './components/Books'
-import NewBook from './components/NewBook'
+import { useState } from 'react';
+import Authors from './components/Authors';
+import Books from './components/Books';
+import NewBook from './components/NewBook';
 
 const App = () => {
-  const [page, setPage] = useState('authors')
+  const [page, setPage] = useState('authors');
 
   return (
     <div>
@@ -20,7 +20,7 @@ const App = () => {
 
       <NewBook show={page === 'add'} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
