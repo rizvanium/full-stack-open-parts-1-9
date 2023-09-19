ffd589190d2913f5010f43179d19a7a9168ea628
Rizvan <rizvan.chalilovas@proton.me>
2023-09-19 10:44:33 +0300

    bloglist-graphql-backend - implement editAuthor mutation for ex.: 8.7

--- part8/books-graphql-backend/index.js
+++ part8/books-graphql-backend/index.js
@@ -110,6 +110,7 @@ const typeDefs = `
       published: Int!
       genres: [String!]!
     ): Book
+    editAuthor(name: String!, setBornTo: Int!): Author
   }
 `
 
@@ -145,6 +146,20 @@ const resolvers = {
       }
 
       return book;
+    },
+    editAuthor: (root, args) => {
+      const author = authors.find(author => author.name === args.name);
+      if (!author) {
+        return null
+      }
+
+      const updatedAuthor = {
+        ...author,
+        born: args.setBornTo
+      }
+
+      authors = authors.map(author => author.name === args.name ? updatedAuthor : author);
+      return updatedAuthor;
     }
   },
 }
