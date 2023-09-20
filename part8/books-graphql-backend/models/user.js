22aac20bc8b18ba6719ce7fbab96c500e3fa6184
Rizvan <rizvan.chalilovas@proton.me>
2023-09-20 23:52:26 +0300

    books-graphql-backend - add user schema

---
+++ part8/books-graphql-backend/models/user.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+var uniqueValidator = require('mongoose-unique-validator');
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    unique: true,
+    required: true,
+    minLength: 3,
+    maxLength: 50,
+  },
+  favoriteGenre: {
+    type: String,
+    required: true,
+  },
+  passwordHash: {
+    type: String,
+    required: true
+  }
+});
+
+userSchema.set('toJSON', {
+  transform: (doc, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject.passwordHash;
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
+
+userSchema.plugin(uniqueValidator);
+
+module.exports = mongoose.model('User', userSchema);
