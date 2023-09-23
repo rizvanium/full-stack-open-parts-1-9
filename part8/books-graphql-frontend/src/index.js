8e84e4141182bfeaa206f77ffdc54e516de61725
Rizvan <rizvan.chalilovas@proton.me>
2023-09-24 00:01:23 +0300

    library-frontend - add graphql subscription support

--- part8/books-graphql-frontend/src/index.js
+++ part8/books-graphql-frontend/src/index.js
@@ -7,8 +7,12 @@ import {
   ApolloProvider,
   InMemoryCache,
   createHttpLink,
+  split,
 } from '@apollo/client';
+import { createClient } from 'graphql-ws';
 import { setContext } from '@apollo/client/link/context';
+import { getMainDefinition } from '@apollo/client/utilities';
+import { GraphQLWsLink } from '@apollo/client/link/subscriptions';
 
 const authLink = setContext((_, { headers }) => {
   const token = localStorage.getItem('library-user-token');
@@ -20,13 +24,27 @@ const authLink = setContext((_, { headers }) => {
   };
 });
 
-const httpLink = createHttpLink({
-  uri: 'http://localhost:4000',
-});
+const httpLink = authLink.concat(
+  createHttpLink({ uri: 'http://localhost:4000' })
+);
+
+const wsLink = new GraphQLWsLink(createClient({ url: 'ws://localhost:4000' }));
+
+const link = split(
+  ({ query }) => {
+    const definition = getMainDefinition(query);
+    return (
+      definition.kind === 'OperationDefinition' &&
+      definition.operation === 'subscription'
+    );
+  },
+  wsLink,
+  httpLink
+);
 
 const client = new ApolloClient({
   cache: new InMemoryCache(),
-  link: authLink.concat(httpLink),
+  link,
 });
 
 ReactDOM.createRoot(document.getElementById('root')).render(
