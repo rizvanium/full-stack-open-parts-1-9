cb585d0577b750bb7288fb90df34c90a519c6f13
Rizvan <rizvan.chalilovas@proton.me>
2023-09-28 21:52:44 +0300

    courseinfo-ts - add more types

--- part9/courseinfo-ts/src/types.tsx
+++ part9/courseinfo-ts/src/types.tsx
@@ -1,4 +1,24 @@
-export interface CoursePart {
+interface CoursePartBase {
   name: string;
   exerciseCount: number;
 }
+
+interface CoursePartDescription extends CoursePartBase {
+  description: string;
+}
+
+export interface CoursePartBasic extends CoursePartDescription {
+  kind: "basic";
+}
+
+export interface CoursePartGroup extends CoursePartBase {
+  groupProjectCount: number;
+  kind: "group";
+}
+
+export interface CoursePartBackground extends CoursePartDescription {
+  backgroundMaterial: string;
+  kind: "background";
+}
+
+export type CoursePart = CoursePartBasic | CoursePartGroup | CoursePartBackground;
