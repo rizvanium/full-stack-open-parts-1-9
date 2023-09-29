a44fc6d6266c84e3ba981e34ebd6a47411aeacaf
Rizvan <rizvan.chalilovas@proton.me>
2023-09-29 12:12:46 +0300

    flight-diary-frontend - implement diary entries view

--- part9/flight-diary-frontend/src/App.tsx
+++ part9/flight-diary-frontend/src/App.tsx
@@ -1,7 +1,25 @@
+import { useEffect, useState } from "react";
+import { DiaryEntry } from "./types";
+import diaryService from "./services/diaryService";
+
 function App() {
+  const [diaries, setDiaries] = useState<DiaryEntry[]>([]);
+
+  useEffect(() => {
+    diaryService.getAll().then(entries => setDiaries(entries));
+  }, []);
+
   return (
     <div>
-      APP
+      <h2>Diary Entries</h2>
+      {diaries.map(entry => <div key={entry.id}>
+        <h3>{entry.date}</h3>
+        <p>visibility: {entry.visibility}
+        </p>
+        <p>
+          weather: {entry.weather}
+        </p>
+      </div>)}
     </div>
   );
 }
--- part9/flight-diary-frontend/src/types.ts
+++ part9/flight-diary-frontend/src/types.ts
@@ -1,1 +1,23 @@
-export type stfu = string;
+export enum Weather {
+  Sunny = 'sunny',
+  Rainy = 'rainy',
+  Cloudy = 'cloudy',
+  Stormy = 'stormy',
+  Windy = 'windy',
+}
+
+export enum Visibility {
+  Great = 'great',
+  Good = 'good',
+  Ok = 'ok',
+  Poor = 'poor',
+}
+
+export interface DiaryEntry {
+  id: number;
+  date: string;
+  weather: Weather;
+  visibility: Visibility;
+}
+
+export type DiaryRequest = Omit<DiaryEntry, 'id'>;
